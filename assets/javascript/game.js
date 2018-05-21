33f5ba03f7e1dd7d571b4e724a25ff0d460342fe
EpicureanHeron <joethor1989@gmail.com>
2018-05-21 14:57:18 -0500

    able to press a button and the guess display updates with ALL of the matching letters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 var userGuess;
 
-var guessedByUser;
+var guessedByUser = [];
 
 var computerGuessArray = ["Troi", "Data", "Picard", "Riker", "Worf", "Crusher", "Enterprise", "Borg", "Caradassians", "Romulans", "Vulcans"];
 
@@ -8,11 +8,46 @@ var computerGuess = computerGuessArray[Math.floor(Math.random() * computerGuessA
 
 var guessState = [];
 
+
+
 for (i = 0; i < computerGuess.length; i++) {
     guessState.push(" _ ");
-
 }
+
+// Updates guess and adds it to the guessedByUser array
+document.onkeyup = function newGuess(event) {
+    userGuess = event.key;
+    guessedByUser.push(userGuess);
+    matchingIndex(userGuess);
+}
+
+function matching(element){
+    if (element == userGuess) {
+        return true;
+
+    }
+    else {
+        return false;
+    }
+}
+
+//Creates an array out of the computer's guess, and cycles through it using the matching function, 
+function matchingIndex(letter){
+    var computerGuessChangedtoArray = computerGuess.split("");
+    for (j = 0; j < computerGuess.length; j++) {
+        if (matching(computerGuessChangedtoArray[j])) {
+            guessState[j] = " " + userGuess + " ";
+            console.log(guessState)
+        }
+    }
+}
+
+// function updateGuessState() {
+//     guessState
+// }
 
 console.log(guessState.toString());
 
 console.log(computerGuess);
+
+console.log(guessedByUser);
